e177d2a2a4815d36b259f179d99fdad2cccc15db
Andy G <andy1.guo@gmail.com>
2022-06-26 12:36:11 -0400

    init

---
+++ engine.h
@@ -0,0 +1,13 @@
+#include "thc.h"
+
+using namespace thc;
+
+class Engine {
+    public:
+
+    Engine();
+
+    double evaluatePosition(ChessPosition);
+
+    
+};
---
+++ main.cpp
@@ -0,0 +1,5 @@
+#include "thc.h"
+
+int main() {
+    // TODO: run update loops
+}
